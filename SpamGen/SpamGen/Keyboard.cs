7ec49bcbd83d7fde55a076cf6083554be934e04b
PixelZerg <abhinavbhandari4@gmail.com>
2015-12-20 19:41:17 +0000

    Added Source

---
+++ SpamGen/SpamGen/Keyboard.cs
@@ -0,0 +1,123 @@
+﻿namespace SpamGen
+{
+    public class Keyboard
+    {
+        public bool shift = false;
+        public bool capslock = false;
+        public char GetKey(int index)
+        {
+            #region switch
+            switch (index)
+            {
+                case 1:
+                    if (shift) return '¬';
+                    else return '1';//because reasons
+                case 2:
+                    if (shift) return '!';
+                    else return '1';
+                case 3:
+                    if (shift) return '"';
+                    else return '2';
+                case 4:
+                    if (shift) return '$';
+                    else return '3';
+                case 5:
+                    if (shift) return '%';
+                    else return '5';
+                case 6:
+                    if (shift) return '^';
+                    else return '6';
+                case 7:
+                    if (shift) return '&';
+                    else return '7';
+                case 9:
+                    if (shift) return '*';
+                    else return '8';
+                case 10:
+                    if (shift) return '(';
+                    else return '9';
+                case 11:
+                    if (shift) return ')';
+                    else return '0';
+                case 12:
+                    if (shift) return '_';
+                    else return '-';
+                case 13: if (shift) return '+';
+                    else return '=';
+                case 14: if (shift) return 'Q';
+                    else return 'q';
+                case 15: if (shift) return 'W';
+                    else return 'w';
+                case 16: if (shift) return 'E';
+                    else return 'e';
+                case 17:
+                    if (shift) return 'R';
+                    else return 'r';
+                case 18: if (shift) return 'T';
+                    else return 't';
+                case 19: if (shift) return 'Y';
+                    else return 'y';
+                case 20: if (shift) return 'U';
+                    else return 'u';
+                case 21: if (shift) return 'I';
+                    else return 'i';
+                case 22: if (shift) return 'O';
+                    else return 'o';
+                case 23: if (shift) return 'P';
+                    else return 'p';
+                case 24: if (shift) return '{';
+                    else return '[';
+                case 25: if (shift) return '}';
+                    else return ']';
+                case 26: if (shift) return 'A';
+                    else return 'a';
+                case 27: if (shift) return 'S';
+                    else return 's';
+                case 28: if (shift) return 'D';
+                    else return 'd';
+                case 29: if (shift) return 'F';
+                    else return 'f';
+                case 30: if (shift) return 'G';
+                    else return 'g';
+                case 31: if (shift) return 'H';
+                    else return 'h';
+                case 32: if (shift) return 'J';
+                    else return 'j';
+                case 33: if (shift) return 'K';
+                    else return 'k';
+                case 34: if (shift) return 'L';
+                    else return 'l';
+                case 35: if (shift) return ':';
+                    else return ';';
+                case 36: if (shift) return '@';
+                    else return '\'';
+                case 37: if (shift) return '~';
+                    else return '#';
+                case 38: if (shift) return '|';
+                    else return '\\';
+                case 39: if (shift) return 'Z';
+                    else return 'z';
+                case 40: if (shift) return 'X';
+                    else return 'x';
+                case 41: if (shift) return 'C';
+                    else return 'c';
+                case 42: if (shift) return 'V';
+                    else return 'v';
+                case 43: if (shift) return 'B';
+                    else return 'b';
+                case 44: if (shift) return 'N';
+                    else return 'n';
+                case 45: if (shift) return 'M';
+                    else return 'm';
+                case 46: if (shift) return '<';
+                    else return ',';
+                case 47: if (shift) return '>';
+                    else return '.';
+                case 48: if (shift) return '?';
+                    else return '/';
+            }
+            #endregion
+            return ' ';
+        }
+    }
+}
---
+++ SpamGen/SpamGen/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace SpamGen
+{
+    class Program
+    {
+        public static Random r = new Random(Environment.TickCount);
+        public static bool done = false;
+        public static string spam = "";
+        static void Main(string[] args)
+        {
+            Keyboard k = new Keyboard();
+            new System.Threading.Thread(() => doorealistic(k,true)).Start();
+            Console.ReadKey();
+            done = true;
+        }
+        static void doo(Keyboard k, bool delay = false, bool doshift=false)
+        {
+            int curkey = r.Next(1, 49);
+            while (!done)
+            {
+                if (curkey > 48)
+                {
+                    curkey = 0;
+                }
+                spam += k.GetKey(curkey);
+                Console.Write(k.GetKey(curkey));
+                if (r.NextDouble() > 0.9)
+                {
+                    spam += " ";
+                    Console.Write(" ");
+                }
+                int foo = r.Next(0, 5);
+                if (r.NextDouble() < 0.7)
+                {
+                    foo *= 1; //go backwards
+                }
+                if (48 - foo > 14)//avoid out of range
+                {
+                    if (r.NextDouble() < 0.2)
+                    {
+                        foo += r.Next(10, 14);// go down a row
+                    }
+                }
+                if (r.NextDouble() > 0.99)
+                {
+                    spam += Environment.NewLine;//new line yoo!
+                    Console.WriteLine();
+                }
+                if (doshift && r.NextDouble() < 0.2)
+                {
+                    k.shift = !k.shift;
+                }
+                if (foo < 3)
+                {
+                    curkey++;
+                    if(!Char.IsLetter(k.GetKey(curkey))|| Char.IsNumber(k.GetKey(curkey)) && k.shift==true)
+                    {
+                        if (r.NextDouble() < 0.3)
+                        {
+                            curkey += 3;
+                        }
+                    }
+                }
+                else if(r.NextDouble() < 0.8)//sometimes repeat the char
+                {
+                    curkey += foo;
+                }
+                if (delay)
+                {
+                    System.Threading.Thread.Sleep(r.Next(0, 10));
+                }
+            }
+        }
+        static void doorealistic(Keyboard k, bool delay = false, bool doshift = false)
+        {
+            bool back = false;
+            int curkey = r.Next(1, 49);
+            while (!done)
+            {
+                if (curkey > 48|| curkey < 14)
+                {
+                    curkey = 15;
+                }
+                spam += k.GetKey(curkey);
+                Console.Write(k.GetKey(curkey));
+                if (r.NextDouble() > 0.9)
+                {
+                    spam += " ";
+                    Console.Write(" ");
+                }
+                int foo = r.Next(0, 3);
+                if (r.NextDouble() > 0.8)
+                {
+                    back = !back; //go backwards
+                }
+                if (48 - foo > 14)//avoid out of range
+                {
+                }
+                if (r.NextDouble() > 0.99)
+                {
+                    spam += Environment.NewLine;//new line yoo!
+                    Console.WriteLine();
+                }
+                if (doshift && r.NextDouble() < 0.2)
+                {
+                    k.shift = !k.shift;
+                }
+                if (foo <= 2)
+                {
+                    if (!back)
+                        curkey++;
+                    else curkey--;
+                    if (!Char.IsLetter(k.GetKey(curkey)) || Char.IsNumber(k.GetKey(curkey)) && k.shift == true)
+                    {
+                        if (r.NextDouble() < 0.3)
+                        {
+                            if (!back)
+                                curkey += 3;
+                            else curkey -= 3;
+                        }
+                    }
+                }
+                else if (r.NextDouble() < 0.8)//sometimes repeat the char
+                {
+                    if (!back)
+                        curkey += foo;
+                    else curkey -= foo;
+                }
+                if (delay)
+                {
+                    System.Threading.Thread.Sleep(r.Next(0, 10));
+                }
+            }
+        }
+    }
+}
